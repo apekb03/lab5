c480abbfd97998c84b5505c4b989fea6bcddbc6b
apekb03 <apekshyabhattarai23@gmail.com>
2025-03-03 20:10:43 -0500

    this is initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express'); // load express module
+const nedb=require('nedb-promises');
+
+const app = express(); // init app
+const db=nedb.create('myfile.json');
+
+app.use(express.static('public')); // enable static routing
+
+// insert and search route
+app.get('/search',(request,response)=>{
+    try{
+        const query=JSON.parse(request.query.find);
+        db.find(query)
+        .then(docs=>{
+            const results = docs.map(docs=>
+                JSON.stringify(docs,null,2)) //can also do for each eg docs.forEach()
+            .join('\n');
+        response.send(results);
+    })
+    }catch(err){
+        response.send('Could not execute query');
+    }
+});
+
+app.get('/insert',(request,response)=>{
+    try{
+        const doc=JSON.parse(request.query.doc);
+        db.insertOne(doc)
+        .then(doc=>{
+            response.send('Inserted:\n'+ JSON.stringify(doc,null,2)) //null ,2 is just to make it nicer when it is printed
+        })
+    }
+    catch(err){
+        response.send('Could not insert document');
+    }
+});
+
+// default route
+
+app.all('*',(request,response)=>{response.status(404).send('Invalid URL.')});
+// start server
+const PORT=3000;
+app.listen( PORT, ()=>console.log('server started… http://localhost:'+ PORT));
---
+++ public/script.js
@@ -0,0 +1,22 @@
+const searchField=document.getElementById('searchField');
+const insertField=document.getElementById('insertField');
+const searchBtn=document.getElementById('searchBtn');
+const insertBtn=document.getElementById('insertBtn');
+const resultsDiv=document.getElementById('results');
+//listens to when search btn is clicked
+searchBtn.addEventListener('click',()=>{
+    fetch('/search?find='+searchField.value) //whenever the search is clicked, it fetches the value in the search box.
+    .then(r=>r.text())
+    .then(txt=>{
+        resultsDiv.innerText=txt
+    })
+});
+
+
+insertBtn.addEventListener('click',()=>{
+    fetch('/insert?doc='+insertField.value) //whenever the search is clicked, it fetches the value in the search box.
+    .then(r=>r.text())
+    .then(txt=>{
+        resultsDiv.innerText=txt
+    });
+});
